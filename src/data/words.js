63eedbba1a68ca5960356e45fa7eb4e65dca9bdd
John <john-ao@hotmail.com>
2024-04-17 22:30:40 -0300

    Tela e botão iniciar

---
+++ src/data/words.js
@@ -0,0 +1,6 @@
+export const wordList = {
+    veiculo:["Carro", "Moto", "Avião", "Trayller", "Ciclomotor","Motocicleta", "Motoneta", "Triciclo", "Quadriciclo", "Charrete", "Ônibus", "Bonde", "Caminhão", "Trem", "Picape", "Carroça"],
+    fruta: ["Maçã", "Banana", "Abacaxi", "Melãncia", "Graviola", "Laranja", "Pêra", "Fruta do Conde", "Manga", "Maracuja", "Caja", "Caju", "Carambola", "Amora", "Uva", "Mamão"],
+    eletronico: ["Walkman", "Discman", "Smartwatch", "Smartphone", "Celular", "Radio", "Autoradio", "Vitrola", "Chapinha", "Secador", "Tablet", "Cópiadora", "Câmera", "Home Theater", "Laptop", "Videogame"],
+    fastfood:["Hamburguer", "X-burguer", "Kibe", "Suco", "Refrigerate", "Batata frita", "Hotdog", "Pizza", "Panqueca", "Torta", "Coxinha", "Milk-shake", "Pastel", "Enroladinho", "Esfirra", "Empada"]
+};
