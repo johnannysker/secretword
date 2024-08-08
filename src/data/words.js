68c137bbde675a59f6a7a0579aad7654a0b2f016
johnannysker <john-ao@hotmail.com>
2024-08-08 00:09:34 -0300

    Telas de reset e gameover

--- src/data/words.js
+++ src/data/words.js
@@ -1,4 +1,4 @@
-export const wordList = {
+export const wordsList = {
     veiculo:["Carro", "Moto", "Avião", "Trayller", "Ciclomotor","Motocicleta", "Motoneta", "Triciclo", "Quadriciclo", "Charrete", "Ônibus", "Bonde", "Caminhão", "Trem", "Picape", "Carroça"],
     fruta: ["Maçã", "Banana", "Abacaxi", "Melãncia", "Graviola", "Laranja", "Pêra", "Fruta do Conde", "Manga", "Maracuja", "Caja", "Caju", "Carambola", "Amora", "Uva", "Mamão"],
     eletronico: ["Walkman", "Discman", "Smartwatch", "Smartphone", "Celular", "Radio", "Autoradio", "Vitrola", "Chapinha", "Secador", "Tablet", "Cópiadora", "Câmera", "Home Theater", "Laptop", "Videogame"],
